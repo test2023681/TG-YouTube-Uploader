0f788d9f7c70f56cd0e9a8dba25eae98d8e7f9d9
ρяє∂αтσя <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-12-16 00:22:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/PredatorHackerzZ/TG-YouTube-Uploader.git /TG-YouTube-Uploader
-cd /AnyDLBot
+cd /TG-YouTube-Uploader
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 main.py
+python3 bot.py
