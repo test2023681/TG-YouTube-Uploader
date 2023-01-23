0096269259bdac7d4648bcc8386a4fc00bdde812
test2023681 <117702231+test2023681@users.noreply.github.com>
2023-01-23 09:09:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5812040555:AAGigOnZCAZHJDkLoOolrAq6CV5d5ZoP6JA")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", '22140808'))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "72e39b383c4655d957f67d4ab94cb3aa")
 
-    AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "")
+    AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "https://te.legra.ph/file/1283821f19752c9187019.jpg")
 
-    VIDEO_THUMBNAIL = os.environ.get("VIDEO_THUMBNAIL", "")
+    VIDEO_THUMBNAIL = os.environ.get("VIDEO_THUMBNAIL", "https://te.legra.ph/file/1283821f19752c9187019.jpg")
     
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
